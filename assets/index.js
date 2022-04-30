96a5c5cd91f2bebddf96bd95e0cd1e054de9509d
Darlyn Norlay <norlaydee@hotmail.co.uk>
2022-04-30 02:09:15 +0100

    Added increment button and more css

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+//document.getElementById("count-el").innerText = 5
+
+
+//STORING THE DATA
+//Setting count to 0 so it starts at 0
+let count = 5
+
+count = count + 10
+
+console.log(count)
+
+
+
+
+
+
